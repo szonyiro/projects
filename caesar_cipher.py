b681cf35d63bd9b5cfd326d9a2f7db59954577aa
szonyiro <97561218+szonyiro@users.noreply.github.com>
2022-02-14 19:34:16 +0000

    Add files via upload

---
+++ caesar_cipher.py
@@ -0,0 +1,38 @@
+from itertools import cycle
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+            ]
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(plain_text, shift_amount):
+    encrypted = ""
+    for letter in plain_text:
+        encrypted += alphabet[alphabet.index(letter) + shift]
+    print(f"\nThe coded text is {encrypted}")
+    return
+
+
+def decrypt(plain_text, shift_amount):
+    decrypted = ""
+    for letter in plain_text:
+        decrypted += alphabet[alphabet.index(letter) - shift]
+    return print(f"\nThe decoded text is {decrypted}")
+
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decrypt(plain_text=text, shift_amount=shift)
+elif direction == "stop":
+    pass
